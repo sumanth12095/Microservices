5ec4887b13b246dbdff2c3b9d37c326069fea574
sumanth pippari <sumanthp@MTLP-1071.in.mouritech.net>
2020-08-13 10:18:21 +0530

    changes

---
+++ PhotoAppApiAccountManagement/src/main/java/com/appdeveloperblog/photoappApi/users/PhotoAppApiAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblog.photoappApi.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
+	}
+
+}
